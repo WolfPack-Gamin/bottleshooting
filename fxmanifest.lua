81d8afe8e9ea64e55edc81ad427dcf45268d3341
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-02-08 17:47:02 -0800

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- fxmanifest.lua
+
+game 'rdr3'
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+client_scripts {
+	'client/util-notify.js',
+	'client/util-client.lua',
+	'client/main-client.lua'
+}
